ee633645bffb5d36c8c2285bf8cab9d6069ab6b2
Chai Feng <chaifeng@chaifeng.com>
2015-03-18 14:39:54 +0800

    Download and verify the cache files

--- provision.d/25-cache.sh
+++ provision.d/25-cache.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 set -e
 
+cd /vagrant/cache
+
+CACHE_SERVER_URL="http://chaifeng.com/vagrant-cache"
 function download_cache() {
-    cd /vagrant/cache
-    if [[ ! -f "$1" ]]; then
-        wget http://chaifeng.com/vagrant-cache/$1
+    FILE="$1"
+
+    if [[ ! -f "$FILE" ]]; then
+        wget -O "$FILE" "${CACHE_SERVER_URL}/$FILE"
     fi
 }
 
-download_cache android-sdk-packages.tgz
-download_cache android-sdk_r23.0.2-linux.tgz
-download_cache apache-ant-1.9.4-bin.tar.gz
-download_cache apache-maven-3.2.3-bin.tar.gz
-download_cache gradle-2.2.1-bin.zip
-download_cache gradle-cache.tgz
-download_cache jdk-8u25-linux-x64.tar.gz
-download_cache latest.tar.gz
-download_cache maven-repository-cache.tgz
+function verify_cache() {
+    FILE_SHA1SUM="${1}.sha1sum"
+    download_cache "$FILE_SHA1SUM"
+    sha1sum --check "$FILE_SHA1SUM"
+    RETVAL=$?
+    [[ $RETVAL -eq 0 ]] || rm "$FILE_SHA1SUM"
+    return $RETVAL
+}
+
+function download_and_verify() {
+    FILE="$1"
+
+    [[ -f "$FILE" ]] && verify_cache "$FILE" && return 0
+    rm -f "$FILE"
+    download_cache "$FILE"
+    verify_cache "$FILE"
+}
+
+download_and_verify android-sdk-packages.tgz
+download_and_verify android-sdk_r23.0.2-linux.tgz
+download_and_verify apache-ant-1.9.4-bin.tar.gz
+download_and_verify apache-maven-3.2.3-bin.tar.gz
+download_and_verify gradle-2.2.1-bin.zip
+download_and_verify gradle-cache.tgz
+download_and_verify jdk-8u25-linux-x64.tar.gz
+download_and_verify latest.tar.gz
+download_and_verify maven-repository-cache.tgz
+
+tar zxf gradle-cache.tgz
+tar zxf maven-repository-cache.tgz
