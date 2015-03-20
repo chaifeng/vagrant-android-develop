bc8e5ff1c79f9120661e17f0f432869783dacaf4
Chai Feng <chaifeng@chaifeng.com>
2015-03-20 10:55:56 +0800

    fix a compatible bug on Windows

--- provision.d/90-wordpress-for-android.sh
+++ provision.d/90-wordpress-for-android.sh
@@ -3,10 +3,14 @@ set -e
 
 echo 'Configure WordPress for Android project ...'
 su -lc /bin/bash vagrant <<EOF
-  set -e
-  WORDPRESS_ANDROID_DIR=\$HOME/src/WordPress-Android
-  if [[ -d \$WORDPRESS_ANDROID_DIR/.git ]] && [[ -f \$WORDPRESS_ANDROID_DIR/WordPress/gradle.properties-example ]]; then
-    [[ -f \$WORDPRESS_ANDROID_DIR/WordPress/gradle.properties ]] || cp -f \$WORDPRESS_ANDROID_DIR/WordPress/gradle.properties-example \$WORDPRESS_ANDROID_DIR/WordPress/gradle.properties
-    [[ -f \$WORDPRESS_ANDROID_DIR/local.properties ]] || echo "sdk.dir=\$ANDROID_HOME" > \$WORDPRESS_ANDROID_DIR/local.properties
+  set -ex
+  cd \$HOME/src/WordPress-Android
+  if [[ -e .git ]] && [[ -f WordPress/gradle.properties-example ]]; then
+    [[ -f WordPress/gradle.properties ]] || cp -f WordPress/gradle.properties-example WordPress/gradle.properties
+    [[ -f local.properties ]] || echo "sdk.dir=\$ANDROID_HOME" > local.properties
+  else
+    echo "You maybe forgot to execute the below command:"
+    echo "    git submodule update --init --recursive"
+    exit 1
   fi
 EOF
