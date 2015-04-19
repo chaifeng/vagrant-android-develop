5802022d795c7f5bb495d699a6016d26bd2e0638
Chai Feng <chaifeng@chaifeng.com>
2015-04-19 18:17:29 +0800

    Use taobao gem source

--- provision.d/50-install-cucumber.sh
+++ provision.d/50-install-cucumber.sh
@@ -13,5 +13,8 @@ su -lc /bin/bash vagrant <<EOF
   grep '.rvm/scripts/rvm' \$HOME/.bashrc || echo '[[ -s "\$HOME/.rvm/scripts/rvm" ]] && source "\$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*' >> \$HOME/.bashrc
   rvm list | fgrep ruby || LC_ALL=C DEBIAN_FRONTEND=noninteractive rvm --quiet-curl install ruby
   rvm use ruby@Calabash-Android --create --default
+  gem sources --remove https://rubygems.org/
+  gem sources -a https://ruby.taobao.org/
+  gem sources --list
   gem install calabash-android --version 0.5.5
 EOF
