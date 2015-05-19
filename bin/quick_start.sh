58e01bc80d79da0174ded24ed652c5dc9d42fa82
Chai Feng <chaifeng@chaifeng.com>
2015-05-19 15:39:30 +0800

    Adding a quick start script

---
+++ bin/quick_start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+error_hook() {
+    cat <<EOF
+
+
+      ______   _____    _____     ____    _____    _   _   _
+     |  ____| |  __ \  |  __ \   / __ \  |  __ \  | | | | | |
+     | |__    | |__) | | |__) | | |  | | | |__) | | | | | | |
+     |  __|   |  _  /  |  _  /  | |  | | |  _  /  | | | | | |
+     | |____  | | \ \  | | \ \  | |__| | | | \ \  |_| |_| |_|
+     |______| |_|  \_\ |_|  \_\  \____/  |_|  \_\ (_) (_) (_)
+
+
+  Please run this command again!
+
+
+EOF
+}
+
+trap error_hook INT TERM ERR
+
+set -eo pipefail
+
+command -V git
+command -V vagrant
+command -V ssh
+
+if [[ "quick_start.sh" == "$(basename "$0")" ]]; then
+    cd "$(dirname "$0")/.."
+fi
+
+[[ -d vagrant-android-develop/.git ]] && cd vagrant-android-develop
+
+if [[ -d .git ]]; then
+    git pull https://github.com/chaifeng/vagrant-android-develop.git || true
+else
+    git clone https://github.com/chaifeng/vagrant-android-develop.git
+    cd vagrant-android-develop
+fi
+git submodule update --init --recursive || true
+
+( vagrant status | grep '^default' | grep -F running && vagrant reload --provision ) || vagrant up --provision
+
+vagrant ssh -c "test -f /.vagrant-shell-provision.DONE"
