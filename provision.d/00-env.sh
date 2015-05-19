5ef32a5c8671e9b82e5649d3688a7052c904354e
Chai Feng <chaifeng@chaifeng.com>
2015-05-19 15:35:50 +0800

    Changing the congratulant message

--- provision.d/00-env.sh
+++ provision.d/00-env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+rm -f /.vagrant-shell-provision.DONE
+
 cat > /etc/profile.d/00-default_vagrant.sh <<EOF
 export VAGRANT_PROJECT_NAME="Android Development"
 EOF
--- provision.d/99-finish.sh
+++ provision.d/99-finish.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+set -e
 
-echo 'Congratulations, the VM is ready!'
-echo 'Use the command `vagrant ssh` to log in the VM.'
+date > /.vagrant-shell-provision.DONE
+cat <<EOF
+
+  ___                        _        _      _   _             _
+ / __|___ _ _  __ _ _ _ __ _| |_ _  _| |__ _| |_(_)___ _ _  __| |
+| (__/ _ \ ' \/ _\` | '_/ _\` |  _| || | / _\` |  _| / _ \ ' \(_-<_|
+ \___\___/_||_\__, |_| \__,_|\__|\_,_|_\__,_|\__|_\___/_||_/__(_)
+              |___/
+
+Congratulations, the VM is ready!
+
+EOF
