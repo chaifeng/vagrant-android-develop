b2fa519588238738e97513968c7dc6209d922af9
Chai Feng <chaifeng@chaifeng.com>
2015-06-17 17:22:42 +0800

    don't use '-o' option for uname

--- bin/unix_quick_start.sh
+++ bin/unix_quick_start.sh
@@ -6,7 +6,7 @@ if [[ "$(id -u)" == "0" ]]; then
     exit 1
 fi
 
-[[ "Darwin" == "$(uname -o)" ]] && if ! brew config; then
+[[ "Darwin" == "$(uname)" ]] && if ! brew config; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
