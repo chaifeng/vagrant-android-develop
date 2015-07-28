edadc0bda42435ada5713365111e3da0f0544acd
Chai Feng <chaifeng@chaifeng.com>
2015-07-28 08:13:22 +0800

    java on raspberrypi

--- provision.d/40-install-jdk.sh
+++ provision.d/40-install-jdk.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -e
 
-apt-get install -y openjdk-7-jdk
+command -V javac || apt-get install -y openjdk-7-jdk
 
-case $(uname -i) in
+case $(uname -m) in
     i386|i686)
         JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386
         ;;
-    *)
+    x86_64)
         JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+        ;;
+    armv7l)
+        JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt
         ;;
 esac
 
-cat > /etc/profile.d/java.sh <<EOF
+if [[ ! -z "$JAVA_HOME" ]]; then
+    cat > /etc/profile.d/java.sh <<EOF
 export JAVA_HOME=$JAVA_HOME
 export PATH=\$JAVA_HOME/bin:\$PATH
 EOF
+fi
